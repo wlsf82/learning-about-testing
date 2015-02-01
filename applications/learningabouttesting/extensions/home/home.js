b23b4dc028d056a675fe2c85c14abfd930a47d91
Joseph Ramos <joseph.ramos@taller.net.br>
2015-02-01 14:06:16 -0500

    Removed home default content.

--- applications/learningabouttesting/extensions/home/home.js
+++ applications/learningabouttesting/extensions/home/home.js
@@ -1,1 +1,13 @@
 var home = module.exports;
+
+/**
+ * The page() hook.
+ */
+home.page = function(pages, callback) {
+
+  // Override home page attributes.
+  pages['home'].title = '';
+  pages['home'].content = '';
+
+  callback();
+};
