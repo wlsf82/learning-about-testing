0fd8140c7fff82c98ae00bc4a7645568255fd1b5
Joseph Ramos <joseph.ramos@taller.net.br>
2015-04-11 22:52:53 -0500

    Updating the application to be more independently - frontend.

---
+++ application/public/app/app.js
@@ -0,0 +1,20 @@
+
+/**
+ * ------------------------------------------------------------------------
+ * Main Application Starter
+ * ------------------------------------------------------------------------
+ * This script is responsible for instantiating the main application's
+ * angular module and grabbing everything together.
+ */
+
+
+// Define the module.
+angular.module('learnig-about-testing',
+
+  /* Application Dependencies
+  --------------------------- */
+  [
+    // Dependencies.
+    'choko'
+  , 'youtube-embed'
+  ]);
